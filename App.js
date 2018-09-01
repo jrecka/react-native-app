ec3fe7344cefce745be46ec42a39877146df9012
Jola <recka.jolanta@gmail.com>
2018-09-01 11:15:49 +0200

    [#1.2]

--- App.js
+++ App.js
@@ -1,27 +1,55 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, FlashList } from 'react-native';
-import {Constans} from 'expo';
-export default class App extends React.Component {
-  
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { Constants } from 'expo';
+
+export default class App extends Component {
+  state = {
+    items: new Array(100).fill(0).map((array, item) => item).map(item => ({
+      title: `Super Title ${item}`,
+      key: item,
+      content: `Super content number ${item}. I made it by myself! Tadam :D`,
+    })),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>Jola are you there?</Text>
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
-      
     );
   }
+
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#33383f',
+    
   },
-  paragraph:{
-    fontSize: 20
-
-  }
+  item: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+    fontSize: 20,
+    color: '#f2f5f9',
+  },
+  content: {
+    marginBottom: 10,
+    color: '#f2f5f9',
+    fontSize: 15,
+  },
 });
